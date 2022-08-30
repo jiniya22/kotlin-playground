09ae6b391095c0c60b9f6ef5fc68dc3c892fbdf8
jini <jini@jiniworld.me>
2022-08-30 23:39:16 +0900

    :tada:

---
+++ src/test/kotlin/xyz/applebox/kotlin/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.applebox.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
