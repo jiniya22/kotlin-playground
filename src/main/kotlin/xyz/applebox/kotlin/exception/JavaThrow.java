3638a8349c7902bde8d461ef0890d57eec06e5cc
jini <jini@jiniworld.me>
2022-09-07 02:20:23 +0900

    :sparkles: 예외처리, 예약어 접근하기

---
+++ src/main/kotlin/xyz/applebox/kotlin/exception/JavaThrow.java
@@ -0,0 +1,24 @@
+package xyz.applebox.kotlin.exception;
+
+import java.io.IOException;
+
+public class JavaThrow {
+    public void throwIOException() throws IOException {
+        throw new IOException("체크드 익셉션인 IOException 발생");
+    }
+
+    public static void main(String[] args) {
+        JavaThrow javaThrow = new JavaThrow();
+        try {
+            javaThrow.throwIOException();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        KotlinThrow kotlinThrow = new KotlinThrow();
+        try {
+            kotlinThrow.throwIOException();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/kotlin/xyz/applebox/kotlin/keyword/KotlinKeyword.kt
@@ -0,0 +1,11 @@
+package xyz.applebox.kotlin.keyword
+
+enum class CountryCodeKotlin {
+    kr, jp, us, `do`;
+}
+
+fun main() {
+    val javaKeyword = JavaKeyword()
+    javaKeyword.`in`
+    javaKeyword.`is`
+}
